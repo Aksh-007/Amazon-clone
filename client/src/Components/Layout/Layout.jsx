541f992d64ceae95d61912293a86f374d3c98204
Aksh-007 <akshaybarapatre54@gmail.com>
2023-07-29 23:40:20 +0530

    Fix - Transition opacity fix

--- client/src/Components/Layout/Layout.jsx
+++ client/src/Components/Layout/Layout.jsx
@@ -3,16 +3,27 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
+  const variants = {
+    hidden: { opacity: 0, x: -800, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+  };
   return (
     <>
       <Header />
 
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 2 }}
-        style={{ minHeight: "780vh", marginTop: "10vh" }}
+        variants={variants} // Pass the variant object into Framer Motion
+        initial="hidden" // Set the initial state to variants.hidden
+        animate="enter" // Animated state to variants.enter
+        exit="exit" // Exit state (used later) to variants.exit
+        transition={{ type: "linear" }} // Set the transition to linear
+        style={{
+          minHeight: "780vh",
+          marginTop: "10vh",
+          display: "flex",
+          justifyContent: "center",
+        }}
       >
         {children}
       </motion.div>
