6d5fbd5b0af17adc7dc9fa0a08e3d9f41a343999
Aksh-007 <akshaybarapatre54@gmail.com>
2023-07-29 23:52:01 +0530

    Fix - Css File added

--- client/src/Components/Layout/Footer.jsx
+++ client/src/Components/Layout/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../Styles/Footer.css";
 const Footer = () => {
   return (
     // <div className="bg-dark text-light p-3 footer">
-    <div className="footer" style={{height:"12vh"}}>
+    <div className="footer" style={{ height: "12vh" }}>
       <h4 className="text-center">All Right Reserved &copy; 3brackets </h4>
       <p className="text-center mt-3 paragraph">
         <Link className="Link" to="/about">
